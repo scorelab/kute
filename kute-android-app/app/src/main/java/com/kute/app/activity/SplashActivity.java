2732bb8b6355683efd77f28e81a56d55546775c5
charith <charithccmc@gmail.com>
2016-06-18 22:56:49 +0530

    Add Splash activity

---
+++ kute-android-app/app/src/main/java/com/kute/app/activity/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.kute.app.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.kute.app.R;
+
+/**
+ * Created by charith on 6/18/16.
+ */
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+    }
+}
