89d8847b8a0328aeeee45d4246b0542475364e9f
charith <charithccmc@gmail.com>
2016-06-18 23:39:40 +0530

    Add Splash Activity almost done and SignIn activity added

---
+++ kute-android-app/app/src/main/java/com/kute/app/activity/SignInActivity.java
@@ -0,0 +1,18 @@
+package com.kute.app.activity;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.kute.app.R;
+
+public class SignInActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signin_activity);
+
+
+    }
+}
--- kute-android-app/app/src/main/java/com/kute/app/activity/SplashActivity.java
+++ kute-android-app/app/src/main/java/com/kute/app/activity/SplashActivity.java
@@ -1,7 +1,11 @@
 package com.kute.app.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.kute.app.R;
 
@@ -10,9 +14,36 @@ import com.kute.app.R;
  */
 public class SplashActivity extends Activity {
 
+    Animation move;
+    ImageView myImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_activity);
+
+        myImageView = (ImageView) findViewById(R.id.myImage);
+        move = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.move);
+        myImageView.startAnimation(move);
+
+        Thread timer = new Thread(){
+            public void run(){
+                try {
+                    sleep(2000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }finally {
+                    Intent openSignInActivity = new Intent("app.android.com.kute.app.SignInActivity");
+                    startActivity(openSignInActivity);
+                }
+            }
+        };
+        timer.start();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 }
