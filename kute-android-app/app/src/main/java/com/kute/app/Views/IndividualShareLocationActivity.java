0449333b50fe5ecf12062d834e9506f27febaa16
NickHub4017 <nrv.rox.u@gmail.com>
2016-08-08 23:26:45 +0530

    remove unwanted imports

--- kute-android-app/app/src/main/java/com/kute/app/Views/IndividualShareLocationActivity.java
+++ kute-android-app/app/src/main/java/com/kute/app/Views/IndividualShareLocationActivity.java
@@ -11,7 +11,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
+
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
