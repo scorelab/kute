26a608c4d8f07b61a829675ec25ef50a50df14ca
dilushi <dilu9218@gmail.com>
2016-08-11 21:47:08 +0530

    add missing files

---
+++ kute-android-app/app/src/main/java/com/kute/app/Activities/SignInActivity.java
@@ -0,0 +1,23 @@
+package com.kute.app.Activities;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.kute.app.R;
+
+public class SignInActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signin_activity);
+
+       /* ImageView splashlogoview=(ImageView)findViewById(R.id.myImage);
+        RelativeLayout mainrelative=(RelativeLayout)findViewById(R.id.splash_mainlayout);
+        splashlogoview.setX(mainrelative.getX()/2);
+        splashlogoview.setY(mainrelative.getY()/2);*/
+
+
+    }
+}
---
+++ kute-android-app/app/src/main/java/com/kute/app/Views/LocationUpdateService.java
@@ -0,0 +1,137 @@
+package com.kute.app.Views;
+
+import android.Manifest;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+
+import com.kute.app.Helpers.DBLatLon;
+
+
+public class LocationUpdateService extends Service {
+
+    private static final String TAG = "BOOMBOOMTESTGPS";
+    private LocationManager locationManager = null;
+    private static final int LOCATION_INTERVAL = 1000;
+    private static final float LOCATION_DISTANCE = 10f;
+    public DBLatLon db;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+
+        if (locationManager == null) {
+            locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+        }
+        try {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                    LOCATION_INTERVAL, LOCATION_DISTANCE, mLocationListeners[1]);
+            Log.d("Dilu Cc", "Called Network");
+        } catch (java.lang.SecurityException ex) {
+            Log.i(TAG, "fail to request location update, ignore", ex);
+        } catch (IllegalArgumentException ex) {
+            Log.d(TAG, "network provider does not exist, " + ex.getMessage());
+        }
+        try {
+            locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER, LOCATION_INTERVAL, LOCATION_DISTANCE,
+                    mLocationListeners[0]);
+            Log.d("Dilu Cc", "GPS");
+        } catch (java.lang.SecurityException ex) {
+            Log.i(TAG, "fail to request location update, ignore", ex);
+        } catch (IllegalArgumentException ex) {
+            Log.d(TAG, "gps provider does not exist " + ex.getMessage());
+        }
+    }
+
+    private class LocationListener implements android.location.LocationListener {
+        Location mLastLocation;
+
+        public LocationListener(String provider) {
+            mLastLocation = new Location(provider);
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+
+            mLastLocation.set(location);
+            Intent intent = new Intent();
+
+            intent.setAction("com.pycitup.BroadcastReceiver");
+            intent.putExtra("Lat", location.getLatitude());
+            intent.putExtra("Lon", location.getLongitude());
+            sendBroadcast(intent);
+            Log.d("Dilu ser", "Service sent a BC");
+            db.addRecord(location.getLatitude(), location.getLongitude());
+        }
+
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    }
+
+    LocationListener[] mLocationListeners = new LocationListener[]{
+            new LocationListener(LocationManager.GPS_PROVIDER),
+            new LocationListener(LocationManager.NETWORK_PROVIDER)
+    };
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (locationManager != null) {
+            for (LocationListener mLocationListener : mLocationListeners) {
+                try {
+                    if (ActivityCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                                != PackageManager.PERMISSION_GRANTED
+                                    && ActivityCompat.checkSelfPermission(this,
+                                        Manifest.permission.ACCESS_COARSE_LOCATION)
+                                            != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    locationManager.removeUpdates(mLocationListener);
+                } catch (Exception ex) {
+                    Log.i(TAG, "fail to remove location listeners, ignore", ex);
+                }
+            }
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+        db = new DBLatLon(getApplicationContext());
+        return START_STICKY;
+    }
+}
