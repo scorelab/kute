30b451a55413c8bf2fad4119586e5fa96aaa5d08
dkarakay <androidkarakay@gmail.com>
2016-12-29 20:48:19 +0300

    DK

---
+++ kute-android-app/app/src/main/java/com/kute/app/Views/Slide05.java
@@ -0,0 +1,21 @@
+package com.kute.app.Views;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kute.app.R;
+
+/**
+ * Created by Deniz on 29.12.2016.
+ */
+
+public class Slide05 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.intro5, container, false);
+        return v;
+    }
+}
