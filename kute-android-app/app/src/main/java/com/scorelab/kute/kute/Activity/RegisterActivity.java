f125c7964138d345cc3177b1c899b120a79c96c8
icancode23 <ernipunarora@gmail.com>
2017-06-19 17:37:43 +0530

    Minor change Switch activity for public and private vehicles ready

--- kute-android-app/app/src/main/java/com/scorelab/kute/kute/Activity/RegisterActivity.java
+++ kute-android-app/app/src/main/java/com/scorelab/kute/kute/Activity/RegisterActivity.java
@@ -157,7 +157,7 @@ public class RegisterActivity extends AppCompatActivity implements
 
             // Configure Google Sign In
             GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                    .requestIdToken(getString(R.string.firebaseGooglesignin))
+                    .requestIdToken(getString(R.string.default_web_client_id))
                     .requestEmail()
                     .build();
             mGoogleApiClient = new GoogleApiClient.Builder(this)
