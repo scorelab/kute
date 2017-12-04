6fa8ab64e73a6df91a4b8746c1db0ea623e08699
azisnaufal <azis.naufal@gmail.com>
2017-12-04 17:17:04 +0700

    Redesign new splash screen

--- kute-android-app/app/src/main/java/com/scorelab/kute/kute/SplashActivity.java
+++ kute-android-app/app/src/main/java/com/scorelab/kute/kute/SplashActivity.java
@@ -5,20 +5,43 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.firebase.database.FirebaseDatabase;
+import com.hanks.htextview.typer.TyperTextView;
 import com.scorelab.kute.kute.Activity.RegisterActivity;
 import com.scorelab.kute.kute.Activity.SwitchPrivatePublicActivity;
 import com.scorelab.kute.kute.Util.ImageHandler;
 
 public class SplashActivity extends AppCompatActivity {
     static  FirebaseDatabase fb;
+    TyperTextView ttv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ttv = (TyperTextView) findViewById(R.id.ttv);
+        ttv.setSoundEffectsEnabled(false);
+        ttv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((TyperTextView)v).animateText("A Commute App for Sri Lanka");
+            }
+        });
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                ttv.performClick();
+            }
+        },2500);
+
+
+
+
 
         new DelayTask().execute(); // This will delay the spalsh scrren and redierct to the login/register screen based
         //on the ststus of the user.
@@ -30,7 +53,7 @@ public class SplashActivity extends AppCompatActivity {
         @Override
         protected Void doInBackground(Void... params) {
             try {
-                Thread.sleep(4000);
+                Thread.sleep(5000);
             }
             catch (Exception e){
 
@@ -49,10 +72,12 @@ public class SplashActivity extends AppCompatActivity {
 
                 Intent regIntent =new Intent(SplashActivity.this, RegisterActivity.class);
                 startActivity(regIntent);
+                finish();
             }
             else{ //User registered in the system.
                 Intent switchIntent =new Intent(SplashActivity.this, SwitchPrivatePublicActivity.class);
                 startActivity(switchIntent);
+                finish();
             }
 
 
