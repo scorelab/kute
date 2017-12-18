c8577800ca6423c0f5ba74840579626cde48ccb3
Raghav Puri <raghavpuri1234567@gmail.com>
2017-12-18 23:52:02 +0530

    Update SplashActivity.java

--- kute-android-app/app/src/main/java/com/scorelab/kute/kute/SplashActivity.java
+++ kute-android-app/app/src/main/java/com/scorelab/kute/kute/SplashActivity.java
@@ -22,7 +22,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
 
         imageView = (ImageView)findViewById(R.id.imageView);
         if(imageView == null) throw new AssertionError();
